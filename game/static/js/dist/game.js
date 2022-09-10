a4b35a403df6e9757521de257122e9db5e7ca8b5
yinshq <yinshq@outlook.com>
2022-09-10 22:49:22 +0800

    create project structure

---
+++ game/static/js/dist/game.js
@@ -0,0 +1,4 @@
+class AcGame {
+    construcor(id) {
+    }
+}
--- scripts/compress_game_js.sh
+++ scripts/compress_game_js.sh
@@ -4,4 +4,4 @@ JS_PATH=/home/ysq/acapp/game/static/js/
 JS_PATH_DIST=${JS_PATH}dist/
 JS_PATH_SRC=${JS_PATH}src/
 
-find $JS_PATH_SRC -type f -name '*.js' | sort | cat > ${JS_PATH_DIST}game.js
+find $JS_PATH_SRC -type f -name '*.js' | sort | xargs cat > ${JS_PATH_DIST}game.js
